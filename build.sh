9566b3af79ecbf3b73a1515e6bc9eb72077e04d0
bladway <shepliakov_vlad@mail.ru>
2025-02-20 02:33:00 +0300

    add autobuild changes, remove vbox

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y virtualbox-ext-pack
 sudo apt install -y vdpauinfo
 sudo apt install -y python3 python3-pip
 sudo apt install -y bc ffmpeg docker.io npm
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt update
+sudo add-apt-repository --yes ppa:graphics-drivers/ppa
+sudo apt update -y
 user=$(whoami)
 sudo usermod -aG docker $user
+exec su -l $user
+export PATH="$PATH:/home/$user/.local/bin"
 npm install
 pip3 install -r python-requirements.txt
 docker pull elgalu/selenium:latest
